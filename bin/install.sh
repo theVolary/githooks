3b953a0fe9ced34ce70e353d68f41154d6b4f421
Steve Krenek <steve.krenek@zyquest.com>
2013-03-25 13:32:47 -0500

    output

--- bin/install.sh
+++ bin/install.sh
@@ -6,7 +6,8 @@ if [ ! $# == 3 ]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+echo "$DIR/../$1/$2"
+echo `env | sort`
 if [ -f "$DIR/../$1/$2" ]; then
   cp "$DIR/../$1/$2" "$3/.git/hooks/$1"
   echo "$1 hook $2 installed in repo $3"
