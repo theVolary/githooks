de157bac203c070a450a1ac4b2d08fc86a2d1d0c
Steve Krenek <steve.krenek@zyquest.com>
2013-03-25 13:25:13 -0500

    Initial-checkin

---
+++ bin/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! $# == 3 ]; then
+  echo "Usage: $0 hook_type hook_name dest_repo_path"
+  exit
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -f "$DIR/../$1/$2" ]; then
+  cp "$DIR/../$1/$2" "$3/.git/hooks/$1"
+  echo "$1 hook $2 installed in repo $3"
+else
+  echo "Unknown hook script $2 of type $1"
+fi
