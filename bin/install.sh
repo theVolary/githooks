1032f6f5a6d779564879b22087c40410ae6ea678
Steve Krenek <steve.krenek@zyquest.com>
2013-03-25 13:41:43 -0500

    fixed DIR

--- bin/install.sh
+++ bin/install.sh
@@ -12,9 +12,15 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-echo "$DIR"
+
+if [ -f "$3" ]; then
+
+else
+  echo "Repo $3 does not exist."
+  exit
+fi
+
 if [ -f "$DIR/../$1/$2" ]; then
-  echo "cp $DIR/../$1/$2 $3/.git/hooks/$1"
   cp "$DIR/../$1/$2" "$3/.git/hooks/$1"
   echo "$1 hook $2 installed in repo $3"
 else
